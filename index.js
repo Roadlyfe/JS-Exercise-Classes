99cae7221a030de669b227f980a8978fa7306bfd
Adam Smith <91632531+Roadlyfe@users.noreply.github.com>
2022-02-17 12:27:03 -0600

    checkcheck

--- index.js
+++ index.js
@@ -198,10 +198,12 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-   constructor(atr{
-     
-   })
+class ProjectManager extends Instructor{
+   constructor(atr){
+     super(atr);
+     this.gradClassName = atr.gradClassName;
+     this.favInstructor = atr.favInstructor;
+   }
 }
 /*
   STRETCH PROBLEM (no tests!)
